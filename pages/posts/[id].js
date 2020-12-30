61a5afaea1d34f8ee2f7804a4ffe43e5df7268b2
Shannon Clarke <shannonajclarke@gmail.com>
2020-12-30 11:39:22 -0400

    chore:fix responsive styles for markdown content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,22 +9,22 @@ const Post = ({ post }) => {
     // console.log(post)
     const photoURL = post.heroImage.fields.file?.url.replace('//', 'http://')
     return (
-        <div className="mx-auto px-4">
+        <div className="w-full mx-auto px-4">
             <Head>
                 <title>{post.title}</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
             </Head>
             <main>
-                <section className="max-w-5xl mx-auto">
+                <section className="w-full mx-auto overflow-hidden">
 
                     <div className="pt-6 pb-8 space-y-2 md:space-y-5 text-center border-b border-gray-100">
                         <p className="text-lg leading-7 text-gray-500">{dayjs(post.publishDate).format("MMMM D, YYYY")}</p>
                         <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">{post.title}</h1>
                     </div>
 
-                    <div className="grid md:grid-cols-4 gap-4">
+                    <div className="flex flex-col md:flex-row">
 
-                        <div className="flex justify-center md:justify-start mt-10 items-start space-x-2 space-y-2">
+                        <div className="md:w-1/4 flex md:justify-center md:justify-start mt-10 items-center md:items-start space-x-2 space-y-2">
                             <img src="/assets/images/headshot_4x.png" className="w-16 h-16 bg-blue-100 rounded-full" />
                             <div>
                                 <p className="text-gray-500 font-medium">Shannon Clarke</p>
@@ -32,8 +32,8 @@ const Post = ({ post }) => {
                             </div>
                         </div>
 
-                        <div className="h-full md:px-10 md:border-l border-gray-100 md:col-span-3 prose max-w-none text-gray-500 my-3 md:my-10 space-y-5">
-                            {photoURL && <img className="w-full rounded mx-auto" src={`${photoURL}`} alt={post.title} />}
+                        <div className="md:w-3/4 overflow-x-auto h-full md:px-10 md:border-l border-gray-100 prose text-gray-500 my-3 md:my-10 space-y-5">
+                            {photoURL && <img className="rounded mx-auto" src={`${photoURL}`} alt={post.title} />}
                             <Markdown plugins={[gfm]} children={post.body} escapeHtml={true} />
                         </div>
                     </div>
