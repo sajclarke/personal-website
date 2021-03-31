fd86ae11dcfd9227de3f701f5ef6289f358abb32
sajclarke <shannonajclarke@gmail.com>
2021-03-31 06:35:39 -0400

    chore: update navbar link to about page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,32 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Header({ title }) {
   return (
     <nav className="bg-white shadow mt-0 sticky z-10 top-0">
-
       <div className="max-w-5xl mx-auto flex justify-between items-center py-2">
         <div className="flex w-full md:w-1/2 justify-center md:justify-start text-white font-semibold">
-          <Link href='/'>
+          <Link href="/">
             <div className="flex text-gray-600  cursor-pointer">
-              <span className="text-xl pl-2"><i className="em em-grinning"></i> Shannon Clarke</span>
+              <span className="text-xl pl-2">
+                <i className="em em-grinning"></i> Shannon Clarke
+              </span>
             </div>
           </Link>
         </div>
         <div className="flex w-full md:w-1/2 justify-end">
           <ul className="list-none">
-
-            {['About'].map((item, index) => (
+            {["About Me"].map((item, index) => (
               <li key={index} className="mr-1">
-                <Link href={`/${item.toLowerCase()}`}><a className="inline-block text-gray-600 hover:bg-indigo-100 hover:text-indigo-500  rounded py-2 px-4">{item}</a></Link>
+                <Link href={`/${item.toLowerCase()}`}>
+                  <a className="inline-block text-gray-600 hover:bg-indigo-100 hover:text-indigo-500  rounded py-2 px-4">
+                    {item}
+                  </a>
+                </Link>
               </li>
             ))}
-
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
