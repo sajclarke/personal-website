799008b831657143f10f414379dfe92abcfed104
sajclarke <shannonajclarke@gmail.com>
2021-12-15 08:18:42 -0400

    fix: corrected the jsx attribute in head

--- components/PageWrapper.js
+++ components/PageWrapper.js
@@ -10,7 +10,7 @@ const PageWrapper = ({ children }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=EDGE" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=EDGE" />
         <meta name="robots" content="index, follow" />
         <meta
           name="keywords"
