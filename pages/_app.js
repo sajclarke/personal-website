cae128c8ca8db012a97522876ee7fd1510b96035
sajclarke <shannonajclarke@gmail.com>
2021-12-15 23:31:34 -0400

    feat: utilize next-seo to set seo keywords and attributes

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,39 @@ import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <DefaultSeo
+        title="Hi, I'm Shannon Clarke. I am passionate about utilizing technology to empower individuals and build communities"
+        description="I believe in a future where modern technology solutions are actively developed by the Caribbean to improve our quality of life."
+        openGraph={{
+          type: "website",
+          locale: "en_US",
+          url: "https://www.shannonclarke.com/",
+          site_name: "Shannon Clarke",
+          description:
+            "I believe in a future where modern technology solutions are actively developed by the Caribbean to improve our quality of life.",
+          images: [
+            {
+              url: "https://shannonclarke.com/assets/images/shannon_block.jpg",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@shannonajclarke",
+          site: "@shannonajclarke",
+          cardType: "summary_large_image",
+        }}
+        additionalMetaTags={[
+          {
+            name: "keywords",
+            content:
+              "shannon clarke, software developer, programming, coding, reactjs, redux, typescript, react, javascript, node, nodejs,barbados",
+          },
+          {
+            httpEquiv: "x-ua-compatible",
+            content: "IE=edge; chrome=1",
+          },
+        ]}
+      />
       <PageWrapper>
         <Component {...pageProps} />
       </PageWrapper>
--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,44 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                    {/* Tracking Codes */}
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-71K3TL2YLR"></script>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* General Stuff */}
+          <title>Shannon Clarke</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            rel="stylesheet"
+          />
+          {/* Tracking Codes */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-71K3TL2YLR"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                                 window.dataLayer = window.dataLayer || [];
                                 function gtag(){dataLayer.push(arguments);}
                                 gtag('js', new Date());
                                 gtag('config', 'G-71K3TL2YLR');
                             `,
-                        }}
-                    />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
